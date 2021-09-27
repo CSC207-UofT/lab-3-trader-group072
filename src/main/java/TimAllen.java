21a87667ef613f8201cfc4a71ec605be3d5a12cc
chili-b <dexter.gaonshatford@gmail.com>
2021-09-27 19:24:35 -0400

    Complete task 2

---
+++ src/main/java/TimAllen.java
@@ -0,0 +1,19 @@
+public class TimAllen implements Tradable, Domesticatable {
+     /**
+     * @return The monetary value of Tim Allen represented by an int.
+     */
+    @Override
+    public int getPrice() {
+        return 3;
+    }
+
+    /**
+     * @return A string representation of the sound Colin Walton makes in the
+     *         intro sequence of Home Improvement.
+     *         (https://www.youtube.com/watch?v=Ji9qSuQapFY)
+     */
+    @Override
+    public String sound() {
+        return "AEUUUGH???";
+    }
+}
