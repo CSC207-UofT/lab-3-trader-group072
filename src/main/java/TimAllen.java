387091a8f02d7c4005bccc74051729b397dff612
chili-b <dexter.gaonshatford@gmail.com>
2021-09-27 20:02:15 -0400

    Apply suggested fixes to TimAllen

--- src/main/java/TimAllen.java
+++ src/main/java/TimAllen.java
@@ -4,11 +4,11 @@ public class TimAllen implements Tradable, Domesticatable {
      */
     @Override
     public int getPrice() {
-        return 4;
+        return 3;
     }
 
     /**
-     * @return A string representation of the sound Colin Walton makes in the
+     * @return A string representation of the sound Tim Allen makes in the
      *         intro sequence of Home Improvement.
      *         (https://www.youtube.com/watch?v=Ji9qSuQapFY)
      */
